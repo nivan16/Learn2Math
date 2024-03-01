d242240410f760725a55fa866a0eb4a1e8bfaecc
nivan2 <naranivan@gmail.com>
2024-03-01 11:11:01 -0500

    altered package json build file

--- config/keys.js
+++ config/keys.js
@@ -4,14 +4,3 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports = require('./keys_dev');
 }
-
-// module.exports = {
-//   mongoURI: 'mongodb+srv://user1:mA9x0EvXck7pyzSc@cluster0.75vat.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//   secretOrKey: 'OB8UgS5r30S0SlZ'
-// }
-// module.exports = {
-//   mongoURI: 'mongodb://<DBUser>:<password>@cluster.mongodb.net/test?retryWrites=true&w=majority'
-//  
-// }
-
-
