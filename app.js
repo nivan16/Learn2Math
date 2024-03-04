22b9d00ec75606c7ab91124afcb6dfa40c1105bd
nivan2 <naranivan@gmail.com>
2024-03-04 04:49:40 -0500

    removed server directory and altered keys import in passport js file

--- app.js
+++ app.js
@@ -13,13 +13,10 @@ const questions = require("./routes/api/questions");
 const quizzes = require("./routes/api/quizzes");
 const quizscores = require("./routes/api/quiz_score");
 const comments = require("./routes/api/comments");
+const User = require("./models/User");
+
 
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('frontend/build'));
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  // })
-
   // Serve the static assets in the frontend's build folder
   app.use(express.static(path.resolve("frontend/build")));
   app.use(cors());
@@ -67,4 +64,13 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV + ' node env');
+
+User.find()
+.then( users => {
+  users.forEach( user => {
+    console.log(user)
+  })
+  console.log(users)
+  console.log("done!")
+})
